42d8fc2c70947e259963b631f2eb784f1f1c91eb
Dawid Dęby <dawid.deby@gmail.com>
2021-02-19 12:01:54 +0100

    generate data and simple validation

--- lib/algorithm_web/live/sort_live.ex
+++ lib/algorithm_web/live/sort_live.ex
@@ -2,7 +2,7 @@ defmodule AlgorithmWeb.SortLive do
   use AlgorithmWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, data: [], min: nil, max: nil)
+    socket = assign(socket, data: [], min: "", max: "")
     {:ok, socket}
   end
 
@@ -12,11 +12,41 @@ defmodule AlgorithmWeb.SortLive do
       "max" => max
     } = params
 
-    assign(socket, min: min, max: max)
+    data = generate_data_or_clear_data(min, max)
+
+    socket =
+      socket
+      |> put_communicate(data)
+      |> assign(
+        min: min,
+        max: max,
+        data: data
+      )
+  end
+
+  defp put_communicate(socket, data) do
+    if length(data) > 0 do
+      socket
+      |> clear_flash()
+      |> put_flash(:info, "New data has been shufled")
+    else
+      socket
+      |> clear_flash()
+      |> put_flash(:error, "Fill min and Max to generate new data")
+    end
+  end
+
+  defp generate_data_or_clear_data(min, max) when min == "" or max == "", do: []
+
+  defp generate_data_or_clear_data(min, max) do
+    min = String.to_integer(min)
+    max = String.to_integer(max)
+    if min < max, do: Enum.shuffle(min..max), else: []
   end
 
   def handle_event("update", params, socket) do
     socket = restart(params, socket)
+    IO.inspect(socket.assigns)
     {:noreply, socket}
   end
 end
